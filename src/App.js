356b0f868622fe40f8a0b271a8a64dfdcdac3f9a
Subhasish Samaddar <subhasish.samaddar@techcrista.com>
2024-09-04 20:05:44 +0530

    implement stopwatch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [time, setTime] = useState(0);
+  const [isStart, seIsStart] = useState(false);
+
+  useEffect(() => {
+    let timerId;
+    if (isStart) {
+      timerId = setInterval(() => setTime(time + 1), 1000);
+    }
+    return () => clearInterval(timerId);
+  }, [isStart, time]);
+
+  const minutes = Math.floor(time / 60);
+  const seconds = Math.floor(time % 60);
+
+  const startStop = () => {
+    seIsStart(!isStart);
+  };
+
+  const reset = () => {
+    setTime(0);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div
+        className="row mt-5"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <div className="card" style={{ width: 500 }}>
+          <div className="card-body">
+            <h5 className="card-title">Stopwatch</h5>
+
+            <p className="card-text">
+              Time : {minutes}:{seconds < 10 ? `0${seconds}` : `${seconds}`}
+            </p>
+            <button className="btn btn-sm btn-success ml-5" onClick={startStop}>
+              {isStart ? "Stop" : "Start"}
+            </button>
+            <button className="btn btn-sm btn-warning mx-2" onClick={reset}>
+              Reset
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
